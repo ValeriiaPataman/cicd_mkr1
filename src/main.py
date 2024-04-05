35c0767dfdef271fb93cc6817b212abd10605002
ValeriiaPataman <valeriia.pataman@knu.ua>
2024-04-05 09:55:32 +0300

    counting words and sentences

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
 
+def count_words_and_sentences(filepath):
+    with open(filepath, 'r') as file:
+        content = file.read()
+
+    words = len(content.split())
+
+    sentences = content.count('.') + content.count('!') + content.count('?')
+
+    sentences -= content.count('...') * 2
+
+    return words, sentences
+
+
+def write_result_to_file(words, sentences, output_file="result.txt"):
+    with open(output_file, 'w') as file:
+        file.write(f"Word count: {words}\n")
+        file.write(f"Sentence count: {sentences}\n")
+
 
 if __name__ == "__main__":
     print('Модульна контрольна робота №1')
     print('Виконала: Патаман Валерія \nГрупа ІПЗ-22')
-    print('Варіант:')
 
+    file_path = "text.txt"
+    words, sentences = count_words_and_sentences(file_path)
+    if words is not None and sentences is not None:
+        print(f"Кількість слів: {words}")
+        print(f"Кількість речень: {sentences}")
+
+        write_result_to_file(words, sentences)
